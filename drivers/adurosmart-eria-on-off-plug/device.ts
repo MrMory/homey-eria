333cdd56aa4c51c9ef8cb2e8d928fecd37d0635b
Florian <f.schaal@caesar.nl>
2023-02-01 19:37:44 +0100

    - Longer interval fetch times - fixes - Implemented settings for some interval fetches

--- drivers/adurosmart-eria-on-off-plug/device.ts
+++ drivers/adurosmart-eria-on-off-plug/device.ts
@@ -1,51 +1,29 @@
-import Homey from 'homey';
 import { ZigBeeDevice } from 'homey-zigbeedriver';
 import { debug, CLUSTER } from 'zigbee-clusters';
-
-// debug(true);
 
 class OnOffPlug extends ZigBeeDevice {
 
   async onNodeInit({ zclNode }: any) {
-    this.registerCapability("onoff", CLUSTER.ON_OFF, {
-      endpoint: 1
-    });
-  }
-  /**
-   * onAdded is called when the user adds the device, called just after pairing.
-   */
-  async onAdded() {
-    this.log('On/Off Plug has been added');
-  }
 
-  /**
-   * onSettings is called when the user updates the device's settings.
-   * @param {object} event the onSettings event data
-   * @param {object} event.oldSettings The old settings object
-   * @param {object} event.newSettings The new settings object
-   * @param {string[]} event.changedKeys An array of keys changed since the previous version
-   * @returns {Promise<string|void>} return a custom message that will be displayed
-   */
-  async onSettings({ oldSettings: {}, newSettings: {}, changedKeys: {} }): Promise<string|void> {
-    this.log('On/Off Plug settings where changed');
-  }
+    // enable debugging
+    //this.enableDebug();
 
-  /**
-   * onRenamed is called when the user updates the device's name.
-   * This method can be used this to synchronise the name to the device.
-   * @param {string} name The new name
-   */
-  async onRenamed(name: string) {
-    this.log('On/Off Plug was renamed');
-  }
+    // Enables debug logging in zigbee-clusters
+    //debug(true);
 
-  /**
-   * onDeleted is called when the user deleted the device.
-   */
-  async onDeleted() {
-    this.log('On/Off Plug has been deleted');
-  }
+    // print the node's info to the console
+    //this.printNode();
 
+    if (this.hasCapability('onoff')) {
+      this.registerCapability('onoff', CLUSTER.ON_OFF, {
+        getOpts: {
+          // If the power plug is controlled by homey the state will be switch directly
+          // If power plug is toggled on the plug itself often this value can be changed to fit the users needs in the settings
+          pollInterval: this.getSetting('report_interval_OnOff') * 1000 || 60000,
+        },
+      });
+    }
+  }
 }
 
 module.exports = OnOffPlug;
--- drivers/adurosmart-eria-power-analytics-plug/device.ts
+++ drivers/adurosmart-eria-power-analytics-plug/device.ts
@@ -1,116 +1,112 @@
-import Homey from 'homey';
-import { ZigBeeDevice } from 'homey-zigbeedriver';
-import { debug, CLUSTER } from 'zigbee-clusters';
-
-// debug(true);
+import {ZigBeeDevice} from 'homey-zigbeedriver';
+import {debug, CLUSTER} from 'zigbee-clusters';
 
 class PowerAnalyticsPlug extends ZigBeeDevice {
 
-  async onNodeInit({ zclNode }: any) {
-    // const meterPowerCluster = await zclNode.endpoints[1].clusters;
-    // console.log(meterPowerCluster);
-    // require('inspector').open(9229, '0.0.0.0');
+    async onNodeInit({zclNode}: any) {
 
-    this.registerCapability("onoff", CLUSTER.ON_OFF);
+        // enable debugging
+        this.enableDebug();
 
-    this.registerCapability("meter_power", CLUSTER.METERING, {
-      get: 'currentSummationDelivered',
-      getOpts: {
-        getOnStart: true,
-        getOnOnline: true,
-        pollInterval: 60000,
-      },
-      report: 'currentSummationDelivered',
-    });
-    
-    // this.registerCapability("meter_power_summed", CLUSTER.METERING, {
-    //   get: 'instantaneousDemand',
-    //   report: 'instantaneousDemand',
-    //   getOpts: {
-    //     getOnStart: true,
-    //     getOnOnline: true,
-    //     pollInterval: 1000,
-    //   },
-    //   endpoint: 1
-    // });
+        // Enables debug logging in zigbee-clusters
+        debug(true);
 
-    this.registerCapability("measure_power", CLUSTER.ELECTRICAL_MEASUREMENT, {
-      get: 'activePower',
-      getOpts: {
-        getOnStart: true,
-        getOnOnline: true,
-        pollInterval: 1000,
-      },
-      report: 'activePower',
-      reportOpts: {
-        configureAttributeReporting: {
-          minInterval: 0,
-          maxInterval: 10,
-          minChange: 1,
+        // print the node's info to the console
+        this.printNode();
+
+        if (this.hasCapability('onoff')) {
+            this.registerCapability('onoff', CLUSTER.ON_OFF, {
+                getOpts: {
+                    // If the power plug is controlled by homey the state will be switch directly
+                    // If power plug is toggled on the plug itself often this value can be changed to fit the users needs in the settings
+                    pollInterval: this.getSetting('report_interval_OnOff') * 1000 || 60000,
+                },
+            });
         }
-      },
-    });
-    
-    this.registerCapability("measure_voltage", CLUSTER.ELECTRICAL_MEASUREMENT, {
-      get: 'rmsVoltage',
-      getOpts: {
-        getOnStart: true,
-        getOnOnline: true,
-        pollInterval: 10000,
-      },
-      report: 'rmsVoltage',
-      reportParser(report: any) {
-        console.log(report);
-        return report/100;
-      },
-    });
 
-    this.registerCapability("measure_current", CLUSTER.ELECTRICAL_MEASUREMENT, {
-      get: 'rmsCurrent',
-      getOpts: {
-        getOnStart: true,
-        getOnOnline: true,
-        pollInterval: 10000,
-      },
-      report: 'rmsCurrent',
-    });
-  }
+        if (this.hasCapability('meter_power')) {
+            this.registerCapability('meter_power', CLUSTER.METERING, {
+                get: 'currentSummationDelivered',
+                getOpts: {
+                    getOnStart: true,
+                    getOnOnline: true,
+                    // In homey the values are registered as kWh.
+                    // This means that a fetch of the data every hour should be enough.
+                    pollInterval: 3600000,
+                },
+                reportOpts: {
+                    configureAttributeReporting: {
+                        minInterval: 120, // No minimum reporting interval
+                        maxInterval: 300, // Maximally every ~16 hours
+                        minChange: 0.01, // Report when value changed by 0.01
+                    },
+                }, report: 'currentSummationDelivered',
+                reportParser(report: number) {
+                    console.log(`reported meter power: ${report}`)
+                    return report / 1000
+                },
+                endpoint: this.getClusterEndpoint(CLUSTER.METERING),
+            });
+        }
 
-  /**
-   * onAdded is called when the user adds the device, called just after pairing.
-   */
-  async onAdded() {
-    this.log('Power Analytics Plug has been added');
-  }
+        if (this.hasCapability('measure_power')) {
+            this.registerCapability('measure_power', CLUSTER.ELECTRICAL_MEASUREMENT, {
+                get: 'activePower',
+                getOpts: {
+                    getOnStart: true,
+                    getOnOnline: true,
+                    // If you are not actively triggering based on power change it's not needed to fetch data every second.
+                    // This can be changed in settings to fit the users needs
+                    pollInterval: this.getSetting('report_interval_measure') * 1000 || 60000,
+                },
+                reportOpts: {
+                    configureAttributeReporting: {
+                        minInterval: 0, // No minimum reporting interval
+                        maxInterval: 300, // Maximally every ~16 hours
+                        minChange: 1, // Report when value changed by 1
+                    },
+                },
+                report: 'activePower',
+                reportParser(report: number) {
+                    console.log(`reported active power: ${report}`)
+                    if(report < 0) return 0;
+                    return report / 10
+                },
+                endpoint: this.getClusterEndpoint(CLUSTER.ELECTRICAL_MEASUREMENT),
+            });
+        }
 
-  /**
-   * onSettings is called when the user updates the device's settings.
-   * @param {object} event the onSettings event data
-   * @param {object} event.oldSettings The old settings object
-   * @param {object} event.newSettings The new settings object
-   * @param {string[]} event.changedKeys An array of keys changed since the previous version
-   * @returns {Promise<string|void>} return a custom message that will be displayed
-   */
-  async onSettings({ oldSettings: {}, newSettings: {}, changedKeys: {} }): Promise<string|void> {
-    this.log('Power Analytics Plug settings where changed');
-  }
+        if (this.hasCapability('measure_voltage')) {
+            this.registerCapability("measure_voltage", CLUSTER.ELECTRICAL_MEASUREMENT, {
+                get: 'rmsVoltage',
+                getOpts: {
+                    getOnStart: true,
+                    getOnOnline: true,
+                    // Voltage almost never changes so every min should be enough
+                    pollInterval: 60000,
+                },
+                report: 'rmsVoltage',
+                reportParser(report: any) {
+                    console.log(`reported voltage: ${report}`)
+                    return report / 100;
+                },
+            });
+        }
 
-  /**
-   * onRenamed is called when the user updates the device's name.
-   * This method can be used this to synchronise the name to the device.
-   * @param {string} name The new name
-   */
-  async onRenamed(name: string) {
-    this.log('Power Analytics Plug was renamed');
-  }
-
-  /**
-   * onDeleted is called when the user deleted the device.
-   */
-  async onDeleted() {
-    this.log('Power Analytics Plug has been deleted');
-  }
-
+        this.registerCapability("measure_current", CLUSTER.ELECTRICAL_MEASUREMENT, {
+            get: 'rmsCurrent',
+            getOpts: {
+                getOnStart: true,
+                getOnOnline: true,
+                pollInterval: 60000,
+            },
+            report: 'rmsCurrent',
+            reportParser(report: any) {
+                console.log(`reported voltage: ${report}`)
+                return report / 10;
+            },
+        });
+    }
 }
 
 module.exports = PowerAnalyticsPlug;
