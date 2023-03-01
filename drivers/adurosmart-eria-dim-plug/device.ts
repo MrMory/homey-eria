303dde615243919dfc2b17478afad30d15c7faf0
Florian Schaal <f.schaal@cloudrepublic.com>
2023-03-01 14:16:41 +0100

    Added functions for setting device capability

--- drivers/adurosmart-eria-dim-plug/device.ts
+++ drivers/adurosmart-eria-dim-plug/device.ts
@@ -14,19 +14,35 @@ class DimPlug extends ZigBeeDevice {
         // print the node's info to the console
         //this.printNode();
 
+        this.registerOnOffCapability(this.getSettings('report_interval_OnOff'));
+
+        if (this.hasCapability('dim')) {
+            this.registerCapability("dim", CLUSTER.LEVEL_CONTROL, {
+                endpoint: 1
+            });
+        }
+    }
+
+    async onSettings({oldSettings, newSettings, changedKeys}: any) {
+
+        try {
+            if (changedKeys.includes('report_interval_OnOff')) {
+                this.registerOnOffCapability(newSettings.report_interval_OnOff);
+            }
+        } catch (err) {
+            // reset settings values on failed update
+            throw new Error(`failed to update settings. Message:${err}`);
+        }
+    }
+
+    registerOnOffCapability(pollInterval: number = 60000) {
         if (this.hasCapability('onoff')) {
             this.registerCapability('onoff', CLUSTER.ON_OFF, {
                 getOpts: {
                     // If the power plug is controlled by homey the state will be switch directly
                     // If power plug is toggled on the plug itself often this value can be changed to fit the users needs in the settings
-                    pollInterval: this.getSetting('report_interval_OnOff') * 1000 || 60000,
+                    pollInterval: pollInterval * 1000 || 60000,
                 },
-            });
-        }
-
-        if (this.hasCapability('dim')) {
-            this.registerCapability("dim", CLUSTER.LEVEL_CONTROL, {
-                endpoint: 1
             });
         }
     }
